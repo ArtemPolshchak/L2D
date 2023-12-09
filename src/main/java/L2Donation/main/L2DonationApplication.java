00e80b5eb73d7a528bcf8f98b31da4181cdcf122
Артем Польщак <artem.polschak@gmail.com>
2023-12-09 16:24:08 +0200

    second commit main

---
+++ src/main/java/L2Donation/main/L2DonationApplication.java
@@ -0,0 +1,13 @@
+package L2Donation.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2DonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2DonationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/L2Donation/main/L2DonationApplicationTests.java
@@ -0,0 +1,13 @@
+package L2Donation.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2DonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
